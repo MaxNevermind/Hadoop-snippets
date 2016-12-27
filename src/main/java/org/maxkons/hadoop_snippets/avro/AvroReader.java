dc212bb2aa7c12bf22212edf01bc7472c877b267
Max <konstantinov.maxim@gmail.com>
2016-12-27 18:18:21 +0300

    refactoring

--- src/main/java/org/maxkons/hadoop_snippets/avro/AvroReader.java
+++ src/main/java/org/maxkons/hadoop_snippets/avro/AvroReader.java
@@ -1,7 +1,9 @@
 package org.maxkons.hadoop_snippets.avro;
 
 import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.AvroFSInput;
@@ -19,11 +21,11 @@ public class AvroReader {
 
     public static void main(String[] args) throws IOException {
         FileSystem fileSystem = FileSystem.newInstance(new Configuration());
-        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(new AvroFSInput(fileSystem.open(IN_PATH), 0), new GenericDatumReader<>());
-        for (GenericRecord datum : dataFileReader) {
-            System.out.println(datum);
+        try (DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(new AvroFSInput(fileSystem.open(IN_PATH), 0), new GenericDatumReader<>())) {
+            for (GenericRecord datum : dataFileReader) {
+                System.out.println(datum);
+            }
         }
-        dataFileReader.close();
     }
 
 }
